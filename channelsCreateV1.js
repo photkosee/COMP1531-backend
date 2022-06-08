cea4cc2613ca5be421640b619813e0ea5a7a083e
Jacinta <z5308921@unsw.edu.au>
2022-06-08 21:52:19 +1000

    created channelsCreateV1.js stub

---
+++ channelsCreateV1.js
@@ -0,0 +1,8 @@
+// Jacinta 8 June 2022
+
+// Stub code for channelsCreateV1
+// Parameters: integer authUserId, string name, boolean isPublic 
+// Temporary Return: concation of parameters
+function channelsCreateV1(authUserId, name, isPublic) {
+    return authUserId + name + isPublic;
+}
