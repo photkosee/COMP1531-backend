c236c665f94897be22803b6cf15d6c17f3522717
Jacinta <z5308921@unsw.edu.au>
2022-06-08 21:29:53 +1000

    created authLoginV1 file

---
+++ authLoginV1.js
@@ -0,0 +1,8 @@
+// Jacinta 8 June 2022
+
+// Stub code for authLoginV1
+// Parameters: string email, string password
+// Temporary Return: concation of parameters
+function authLoginV1(email, password) {
+    return email + password;
+}
