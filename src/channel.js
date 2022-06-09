08d4016863aac4214a0dccbe24b7ce1325042088
devmridul <devmridulsingh@gmail.com>
2022-06-09 16:59:57 +1000

    Implemented function channelDetailsV1 in channel.js

---
+++ src/channel.js
@@ -0,0 +1,9 @@
+/* 
+@param {authUserId} is a integer
+@param {channelId} is a integer
+@returns {string} concatenating of params
+*/
+
+function channelDetailsV1(authUserId, channelId) {
+    return 'authUserId' + 'channelId';
+};
