73d3f777703c04cdbf2e5944f1efaadd27eb8188
Jacinta <z5308921@unsw.edu.au>
2022-06-09 16:03:57 +1000

    added authRegisterV1 to auth.js

---
+++ src/auth.js
@@ -0,0 +1,8 @@
+// Jacinta 9 June 2022
+
+// Stub code for authRegisterV1
+// Parameters: string email, string password, string nameFirst, string nameLast 
+// Temporary Return: concation of parameters
+function authRegisterV1(email, password, nameFirst, nameLast) {
+    return email + password + nameFirst + nameLast;
+}
