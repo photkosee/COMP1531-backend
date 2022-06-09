3f26deaea1e20b56a4843c1cb32f620aa37920bc
Luke Yang <z5416119@cse.unsw.edu.au>
2022-06-09 11:47:31 +1000

    Added channelsListAllV1 funct to channels.js

---
+++ src/channels.js
@@ -0,0 +1,7 @@
+//Luke Yang 9/6/2022
+//Stub function for channelsListAllV1
+//Parameters: int authUserId
+//Stub Returns: int authUserId
+function channelsListAllV1(authUserId) {
+    return authUserId;
+}
