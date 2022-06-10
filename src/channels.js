2b8ad5c1e73e6b8d189ba6feca2a4bb96a8e86be
Phot <z5387411@ad.unsw.edu.au>
2022-06-10 11:21:33 +1000

    Commenting and rearranging all functions

--- src/channels.js
+++ src/channels.js
@@ -1,24 +1,45 @@
-
-//Luke Yang 9/6/2022
-//Stub function for channelsListAllV1
-//Parameters: int authUserId
-//Stub Returns: int authUserId
 function channelsListAllV1(authUserId) {
+	/*
+		Function channelsListAllV1 (waiting for details in future tasks)
+		
+		Arguments:
+			authUserId	integer type   -- Input integer supplied by user
+			
+		Return Value:
+			string: of authUserId
+			
+	*/
     return 'authUserId';
-}
-//Stub function code for channelsListV1
-//Parameters: int authUserId
-//Stub Returns: int authUserId
+};
+
+
 function channelsListV1(authUserId) {
+	/*
+		Function channelsListV1 (waiting for details in future tasks)
+		
+		Arguments:
+			authUserId	integer type   -- Input integer supplied by user
+			
+		Return Value:
+			string: of authUserId
+			
+	*/
     return 'authUserId'; 
-}
+};
 
-// Jacinta 9 June 2022
-// Stub code for channelsCreateV1
-// Parameters: integer authUserId, string name, boolean isPublic 
-// Temporary Return: concation of parameters
+
 function channelsCreateV1(authUserId, name, isPublic) {
+	/*
+		Function channelsCreateV1 (waiting for details in future tasks)
+		
+		Arguments:
+			authUserId	integer type    -- Input integer supplied by user
+			name		string  type	-- Input string supplied by user	
+			isPublic	boolean type	-- Input boolean supplied by user
+			
+		Return Value:
+			string: a combined of authUserId, name and isPublic
+			
+	*/
     return 'authUserId' + 'name' + 'isPublic';
-
-
-}
+};
