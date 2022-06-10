16585871f5c07170d4ce1da09cb877fd2fb71c9c
devmridul <devmridulsingh@gmail.com>
2022-06-10 10:30:21 +1000

    Added function channelDetailsV1

--- src/channel.js
+++ src/channel.js
@@ -7,3 +7,13 @@
 function channelDetailsV1(authUserId, channelId) {
     return 'authUserId' + 'channelId';
 };
+
+/* 
+@param {authUserId} is a integer
+@param {channelId} is a integer
+@returns {string} concatenating of params
+*/
+
+function channelJoinV1(authUserId, channelId) {
+    return 'authUserId' + 'channelId';
+};
