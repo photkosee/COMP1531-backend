70b934f87784cfeabfa9f4a60120251f3631469a
Jacinta <z5308921@unsw.edu.au>
2022-06-15 21:12:32 +1000

    fixed valid return type

--- src/channel.test.js
+++ src/channel.test.js
@@ -9,39 +9,44 @@ import { clearV1 } from './other.js';
 
 
 test ('channelDetailsV1: valid userId and valid channelId', () => {
-    clearAndSetData();
+    const channelId = clearAndSetData();
 
-    const dataExpected = expectValid();
-
-    expect(channelDetailsV1(1, 10)).toMatchObject(dataExpected);
+    expect(channelDetailsV1(1, channelId)).toMatchObject(
+        {
+        'name': 'FO9A_CRUNCHIE',
+        'isPublic': false,
+        'ownerMembers': [ 1 ],
+        'allMembers': [ 1, 2 ],
+        }
+    );
 });
 
 test ('channelDetailsV1: valid userId and non-existent channelId', () => {
-    clearAndSetData();
+    const channelId = clearAndSetData();
 
     expect(channelDetailsV1(1, 99)).toMatchObject({error: 'error'});
 });
 
 test ('channelDetailsV1: non-existent userId and valid channelId', () => {
-    clearAndSetData();
+    const channelId = clearAndSetData();
 
     expect(channelDetailsV1(9, 10)).toMatchObject({error: 'error'});
 });
 
 test ('channelDetailsV1: invalid userId and valid channelId', () => {
-    clearAndSetData();
+    const channelId = clearAndSetData();
 
     expect(channelDetailsV1('abc', 10)).toMatchObject({error: 'error'});
 });
 
 test ('channelDetailsV1: valid userId and invalid channelId', () => {
-    clearAndSetData();
+    const channelId = clearAndSetData();
 
     expect(channelDetailsV1(1, 'abc')).toMatchObject({error: 'error'});
 });
 
 test ('channelDetailsV1: valid userId but not member', () => {
-    clearAndSetData();
+    const channelId = clearAndSetData();
 
     expect(channelDetailsV1(3, 10)).toMatchObject({error: 'error'});
 });
@@ -50,29 +55,17 @@ function clearAndSetData() {
 
     clearV1();
 
-    // Users 1 and 3
-    const authUserId1 = authRegisterV1('z1111@gmail.com', 'one', 'One', 'Number');
-    const authUserId2 = authRegisterV1('z2222@gmail.com', 'two', 'Two', 'Number');
-    const authUserId3 = authRegisterV1('z3333', 'three', 'Three', 'Number');
+    // Users 1, 2 and 3
+    const authUserId1 = authRegisterV1('mal1@', 'one', 'One', 'Number');
+    const authUserId2 = authRegisterV1('mal2@', 'two', 'Two', 'Number');
+    const authUserId3 = authRegisterV1('mal3@', 'three', 'Three', 'Number');
 
     // Create channel FO9A_CRUNCHIE
+    // Admin: User 1
+    // Members: User 1, User 2
     const channelId = channelsCreateV1(authUserId1, 'FO9A_CRUNCHIE', false);
     channelJoinV1(authUserId2, channelId);
+
+    return channelId;
 }
 
-function expectValid() {
-
-    const data = getData();
-
-    const expectedData = data.channel[1];
-    return expectedData;
-
-/*
-    {
-        'name': 'FO9A_CRUNCHIE',
-        'isPublic': false,
-        'ownerMembers': [ 1 ],
-        'allMembers': [ 1, 2 ],
-    }
-*/
-}
