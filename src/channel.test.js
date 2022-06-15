35ef4a75f534a0ace2d9d090b232a8902a7cf9a6
Jacinta <z5308921@unsw.edu.au>
2022-06-15 15:11:20 +1000

    changed expected and added getData

--- src/channel.test.js
+++ src/channel.test.js
@@ -19,41 +19,31 @@ test ('channelDetailsV1: valid userId and valid channelId', () => {
 test ('channelDetailsV1: valid userId and non-existent channelId', () => {
     clearAndSetData();
 
-    const dataExpected = expectError();
-
-    expect(channelDetailsV1(1, 99)).toMatchObject(dataExpected);
+    expect(channelDetailsV1(1, 99)).toMatchObject({error: 'error'});
 });
 
 test ('channelDetailsV1: non-existent userId and valid channelId', () => {
     clearAndSetData();
 
-    const dataExpected = expectError();
-
-    expect(channelDetailsV1(9, 10)).toMatchObject(dataExpected);
+    expect(channelDetailsV1(9, 10)).toMatchObject({error: 'error'});
 });
 
 test ('channelDetailsV1: invalid userId and valid channelId', () => {
     clearAndSetData();
 
-    const dataExpected = expectError();
-
-    expect(channelDetailsV1('abc', 10)).toMatchObject(dataExpected);
+    expect(channelDetailsV1('abc', 10)).toMatchObject({error: 'error'});
 });
 
 test ('channelDetailsV1: valid userId and invalid channelId', () => {
     clearAndSetData();
 
-    const dataExpected = expectError();
-
-    expect(channelDetailsV1(1, 'abc')).toMatchObject(dataExpected);
+    expect(channelDetailsV1(1, 'abc')).toMatchObject({error: 'error'});
 });
 
 test ('channelDetailsV1: valid userId but not member', () => {
     clearAndSetData();
 
-    const dataExpected = expectError();
-
-    expect(channelDetailsV1(3, 10)).toMatchObject(dataExpected);
+    expect(channelDetailsV1(3, 10)).toMatchObject({error: 'error'});
 });
 
 function clearAndSetData() {
@@ -62,24 +52,27 @@ function clearAndSetData() {
 
     // Users 1 and 3
     const authUserId1 = authRegisterV1('z1111@gmail.com', 'one', 'One', 'Number');
+    const authUserId2 = authRegisterV1('z2222@gmail.com', 'two', 'Two', 'Number');
     const authUserId3 = authRegisterV1('z3333', 'three', 'Three', 'Number');
 
     // Create channel FO9A_CRUNCHIE
     const channelId = channelsCreateV1(authUserId1, 'FO9A_CRUNCHIE', false);
-    channelJoinV1(authUserId3, channelId);
+    channelJoinV1(authUserId2, channelId);
 }
 
 function expectValid() {
-    return {
+
+    const data = getData();
+
+    const expectedData = data.channel[1];
+    return expectedData;
+
+/*
+    {
         'name': 'FO9A_CRUNCHIE',
         'isPublic': false,
         'ownerMembers': [ 1 ],
         'allMembers': [ 1, 2 ],
     }
-}
-
-function expectError() {
-    return {
-        error: 'error',
-    }
+*/
 }
