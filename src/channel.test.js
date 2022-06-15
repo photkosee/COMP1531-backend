c47e30ca11ac99ad68e408620292cdd2332cc390
Jacinta <z5308921@unsw.edu.au>
2022-06-15 14:19:39 +1000

    Draft of channelDetailsV1 test

--- src/channel.test.js
+++ src/channel.test.js
@@ -1,9 +1,61 @@
+// Jacinta 15 June 2020
+// channelDetailsV1 Jest tests
+
 import { channelDetailsV1 } from './channel.js';
 import { getData, setData } from './dataStore'; 
 import { clearV1 } from './other';
 
 
 test ('channelDetailsV1: valid userId and valid channelId', () => {
+    clearAndSetData();
+
+    const dataExpected = expectValid();
+
+    expect(channelDetailsV1(1, 10)).toMatchObject(dataExpected);
+});
+
+test ('channelDetailsV1: valid userId and non-existent channelId', () => {
+    clearAndSetData();
+
+    const dataExpected = expectError();
+
+    expect(channelDetailsV1(1, 99)).toMatchObject(dataExpected);
+});
+
+test ('channelDetailsV1: non-existent userId and valid channelId', () => {
+    clearAndSetData();
+
+    const dataExpected = expectError();
+
+    expect(channelDetailsV1(9, 10)).toMatchObject(dataExpected);
+});
+
+test ('channelDetailsV1: invalid userId and valid channelId', () => {
+    clearAndSetData();
+
+    const dataExpected = expectError();
+
+    expect(channelDetailsV1('abc', 10)).toMatchObject(dataExpected);
+});
+
+test ('channelDetailsV1: valid userId and invalid channelId', () => {
+    clearAndSetData();
+
+    const dataExpected = expectError();
+
+    expect(channelDetailsV1(1, 'abc')).toMatchObject(dataExpected);
+});
+
+test ('channelDetailsV1: valid userId but not member', () => {
+    clearAndSetData();
+
+    const dataExpected = expectError();
+
+    expect(channelDetailsV1(3, 10)).toMatchObject(dataExpected);
+});
+
+function clearAndSetData() {
+
     clearV1();
 
     const data = {
@@ -11,6 +63,9 @@ test ('channelDetailsV1: valid userId and valid channelId', () => {
             {
                 'authUserId': 1,
             },
+            {
+                'autherUserId': 3,
+            }
         ],
         'channels': [
             {
@@ -24,32 +79,20 @@ test ('channelDetailsV1: valid userId and valid channelId', () => {
     };
     setData(data);
 
-    const dataExpected = {
+}
+
+
+function expectValid() {
+    return {
         'name': 'FO9A_CRUNCHIE',
         'isPublic': false,
         'ownerMembers': [ 1 ],
         'allMembers': [ 1, 2 ],
     }
+}
 
-    expect(channelDetailsV1(1, 10)).toMatchObject(dataExpected);
-});
-
-test ('channelDetailsV1: valid userId and non-existent channelId', () => {
-    
-});
-
-test ('channelDetailsV1: non-existent userId and valid channelId', () => {
-    
-});
-
-test ('channelDetailsV1: invalid userId and valid channelId', () => {
-    
-});
-
-test ('channelDetailsV1: valid userId and invalid channelId', () => {
-    
-});
-
-test ('channelDetailsV1: valid userId but not member', () => {
-    
-});
+function expectError() {
+    return {
+        error: 'error',
+    }
+}
