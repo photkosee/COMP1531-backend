f7dc46f32a22f0c10c28f0320d6ed6f318af7643
Phot <z5387411@ad.unsw.edu.au>
2022-06-17 01:03:04 +1000

    tests

--- src/channels.test.js
+++ src/channels.test.js
@@ -9,7 +9,9 @@ test('Testing creating channels', () => {
     const authUserId = authRegisterV1('mal@gmail.com', '123456', 'first', 'last');
     expect(typeof authUserId).toBe('integer');
     const channelId = channelsCreateV1(authUserId, 'name', 'true');
+    const channelId2 = channelsCreateV1(authUserId, 'name', 'true');
     expect(typeof channelId).toBe('integer');
+    expect(typeof channelId2).toBe('integer');
 }); 
 
 test('Testing error in creating channels by having no names or more than 20 characters', () => {
@@ -21,104 +23,3 @@ test('Testing error in creating channels by having no names or more than 20 char
     expect(channelId_a).toMatchObject({ error: 'error' });
     expect(channelId_b).toMatchObject({ error: 'error' });
 }); 
-
-test('Testing listing channels', () => {
-    clearV1();
-    validator.isEmail('mal@gmail.com');
-    validator.isEmail('mal2@gmail.com');
-    validator.isEmail('mal3@gmail.com');
-    validator.isEmail('mal4@gmail.com');
-    const authUserId_1 = authRegisterV1('mal@gmail.com', '123456', 'first', 'last');
-    const authUserId_2 = authRegisterV1('mal2@gmail.com', '123456', 'first2', 'last2');
-    const authUserId_3 = authRegisterV1('mal3@gmail.com', '123456', 'first3', 'last3');
-    const authUserId_4 = authRegisterV1('mal4@gmail.com', '123456', 'first4', 'last4');
-    const channelId_1 = channelsCreateV1(authUserId_1, 'pete', false);
-    const channelId_2 = channelsCreateV1(authUserId_2, 'jay', true);
-    const channelId_3 = channelsCreateV1(authUserId_3, 'jay', true);
-    const channelId_4 = channelsCreateV1(authUserId_4, 'mark', false);
-    const owner_pete = channelsListV1(authUserId_1);
-    const owner_jay = channelsListV1(authUserId_2);
-    const owner_jay2 = channelsListV1(authUserId_3);
-    expect(owner_pete).toMatchObject({
-					channels: [
-                                   	  { 
-					    channelId: channelId_1, 
-					    channelName: 'pete' 
-					  }
-                                        ]  
-				   });
-    expect(owner_jay).toMatchObject({
-					channels: [                                  	  
-                                   	  { 
-					    channelId: channelId_2, 
-					    channelName: 'jay' 
-					  }
-                                        ]  
-				   });  
-    expect(owner_jay2).toMatchObject({
-					channels: [                                  	  
-                                   	  { 
-					    channelId: channelId_3, 
-					    channelName: 'jay' 
-					  }
-                                        ]  
-				   });                               
-});
-
-test('Testing listing all channels', () => {
-    clearV1();
-    validator.isEmail('mal@gmail.com');
-    validator.isEmail('mal2@gmail.com');
-    validator.isEmail('mal3@gmail.com');
-    validator.isEmail('mal4@gmail.com');
-    const authUserId_1 = authRegisterV1('mal@gmail.com', '123456', 'first', 'last');
-    const authUserId_2 = authRegisterV1('mal2@gmail.com', '123456', 'first2', 'last2');
-    const authUserId_3 = authRegisterV1('mal3@gmail.com', '123456', 'first3', 'last3');
-    const authUserId_4 = authRegisterV1('mal4@gmail.com', '123456', 'first4', 'last4');
-    const channelId_1 = channelsCreateV1(authUserId_1, 'pete', false);
-    const channelId_2 = channelsCreateV1(authUserId_2, 'jay', true);
-    const channelId_3 = channelsCreateV1(authUserId_3, 'may', true);
-    const channelId_4 = channelsCreateV1(authUserId_4, 'mark', false);
-    const owner_pete = channelsListallV1(authUserId_1);
-    const owner_jay = channelsListallV1(authUserId_2);
-    expect(owner_pete).toMatchObject({
-					channels: [
-                                   	  { 
-					    channelId: channelId_1, 
-					    channelName: 'pete' 
-					  },
-                                   	  { 
-					    channelId: channelId_2, 
-					    channelName: 'jay' 
-					  },
-					  { 
-					    channelId: channelId_3, 
-					    channelName: 'may' 
-					  },
-                                   	  { 
-					    channelId: channelId_4, 
-					    channelName: 'mark' 
-					  }
-                                        ]  
-				   });
-    expect(owner_jay).toMatchObject({
-					channels: [
-                                   	  { 
-					    channelId: channelId_1, 
-					    channelName: 'pete' 
-					  },
-                                   	  { 
-					    channelId: channelId_2, 
-					    channelName: 'jay' 
-					  },
-					  { 
-					    channelId: channelId_3, 
-					    channelName: 'may' 
-					  },
-                                   	  { 
-					    channelId: channelId_4, 
-					    channelName: 'mark' 
-					  }
-                                        ]  
-				   });                        
-});
