19b574d5af4ae5ecd60671f0604b97aa09e42d45
Luke Yang <z5416119@cse.unsw.edu.au>
2022-06-16 13:31:16 +1000

    Changed function return values from objects to function.Id

--- src/channel.test.js
+++ src/channel.test.js
@@ -28,15 +28,15 @@ describe ('Test cases for channelInviteV1', () => {
     test ('Valid Channel and Valid User Ids', () => {
         clearV1();
 
-        const authUserId1 = authRegisterV1('user1@bar.com', '123456', 'first1', 'last1'); //in
-        const authUserId2 = authRegisterV1('user2@bar.com', '123456', 'first2', 'last2'); //out
-        const authUserId3 = authRegisterV1('user3@bar.com', '123456', 'first3', 'last3'); //in
-        const authUserId4 = authRegisterV1('user4@bar.com', '123456', 'first4', 'last4'); //in
-        const authUserId5 = authRegisterV1('user5@bar.com', '123456', 'first5', 'last5'); //out
-        const authUserId6 = authRegisterV1('user6@bar.com', '123456', 'first6', 'last6'); //out
-        const authUserId7 = authRegisterV1('user7@bar.com', '123456', 'first7', 'last7'); //out
-        const authUserId8 = authRegisterV1('user8@bar.com', '123456', 'first8', 'last8'); //in
-        const channel1 = channelsCreateV1(authUserId1, 'channel1', true); 
+        const authUserId1 = authRegisterV1('user1@bar.com', '123456', 'first1', 'last1').authUserId; //in
+        const authUserId2 = authRegisterV1('user2@bar.com', '123456', 'first2', 'last2').authUserId; //out
+        const authUserId3 = authRegisterV1('user3@bar.com', '123456', 'first3', 'last3').authUserId; //in
+        const authUserId4 = authRegisterV1('user4@bar.com', '123456', 'first4', 'last4').authUserId; //in
+        const authUserId5 = authRegisterV1('user5@bar.com', '123456', 'first5', 'last5').authUserId; //out
+        const authUserId6 = authRegisterV1('user6@bar.com', '123456', 'first6', 'last6').authUserId; //out
+        const authUserId7 = authRegisterV1('user7@bar.com', '123456', 'first7', 'last7').authUserId; //out
+        const authUserId8 = authRegisterV1('user8@bar.com', '123456', 'first8', 'last8').authUserId; //in
+        const channel1 = channelsCreateV1(authUserId1, 'channel1', true).channelId; 
         channelJoinV1(authUserId3, channel1);
         channelJoinV1(authUserId4, channel1);  
         channelJoinV1(authUserId8, channel1);
@@ -51,11 +51,11 @@ describe ('Test cases for channelInviteV1', () => {
 
     test ('Valid Channel and Invalid uId', () => {
         clearV1(); 
-        const authUserId1 = authRegisterV1('user1@bar.com', '123456', 'first1', 'last1'); 
-        const authUserId2 = authRegisterV1('user2@bar.com', '123456', 'first2', 'last2'); 
-        const authUserId6 = authRegisterV1('user6@bar.com', '123456', 'first6', 'last6'); 
-        const authUserId8 = authRegisterV1('user8@bar.com', '123456', 'first8', 'last8'); 
-        const channel1 = channelsCreateV1(authUserId1, 'channel1', true); 
+        const authUserId1 = authRegisterV1('user1@bar.com', '123456', 'first1', 'last1').authUserId; 
+        const authUserId2 = authRegisterV1('user2@bar.com', '123456', 'first2', 'last2').authUserId; 
+        const authUserId6 = authRegisterV1('user6@bar.com', '123456', 'first6', 'last6').authUserId; 
+        const authUserId8 = authRegisterV1('user8@bar.com', '123456', 'first8', 'last8').authUserId; 
+        const channel1 = channelsCreateV1(authUserId1, 'channel1', true).channelId; 
         channelJoinV1(authUserId8, channel1); 
         expect(channelInviteV1(authUserId1, channel1, -1)).toMatchObject({ error: 'error' }); // valid invalid -- in channel
         expect(channelInviteV1(authUserId2, channel1, -1)).toMatchObject({ error: 'error' }); // valid invalid -- not
@@ -67,10 +67,10 @@ describe ('Test cases for channelInviteV1', () => {
 
     test ('Invalid Channel', () => {
         clearV1(); 
-        const authUserId1 = authRegisterV1('user1@bar.com', '123456', 'first1', 'last1'); 
-        const authUserId2 = authRegisterV1('user2@bar.com', '123456', 'first2', 'last2'); 
-        const authUserId4 = authRegisterV1('user4@bar.com', '123456', 'first4', 'last4'); 
-        const authUserId6 = authRegisterV1('user6@bar.com', '123456', 'first6', 'last6'); 
+        const authUserId1 = authRegisterV1('user1@bar.com', '123456', 'first1', 'last1').authUserId; 
+        const authUserId2 = authRegisterV1('user2@bar.com', '123456', 'first2', 'last2').authUserId; 
+        const authUserId4 = authRegisterV1('user4@bar.com', '123456', 'first4', 'last4').authUserId; 
+        const authUserId6 = authRegisterV1('user6@bar.com', '123456', 'first6', 'last6').authUserId; 
         
         expect(channelInviteV1(authUserId1, -1, -1)).toMatchObject({ error: 'error' }); // valid invalid 
         expect(channelInviteV1(authUserId2, -1, authUserId4)).toMatchObject({ error: 'error' }); // valid valid 
@@ -83,13 +83,13 @@ describe ('Test cases for channelInviteV1', () => {
     
     test ('uId of authUserId is same as uId invite', () => {
         clearV1();
-        const authUserId1 = authRegisterV1('user1@bar.com', '123456', 'first1', 'last1'); 
-        const authUserId2 = authRegisterV1('user2@bar.com', '123456', 'first2', 'last2'); 
-        const channel1 = channelsCreateV1(authUserId1, 'channel1', true); 
+        const authUserId1 = authRegisterV1('user1@bar.com', '123456', 'first1', 'last1').authUserId; 
+        const authUserId2 = authRegisterV1('user2@bar.com', '123456', 'first2', 'last2').authUserId; 
+        const channel1 = channelsCreateV1(authUserId1, 'channel1', true).channelId; 
 
         expect(channelInviteV1(authUserId1, channel1, authUserId1)).toMatchObject({ error: 'error' }); // in channel
         expect(channelInviteV1(authUserId2, channel1, authUserId2)).toMatchObject({ error: 'error' }); // not in channel
-
+        expect(channelInviteV1(-1,channel1,-1)).toMatchObject({ error: 'error' }); 
 
     });
 
