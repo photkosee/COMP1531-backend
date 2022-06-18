6a4f8b9d8ee77e5d1230b260ea0e2e38a8defa0c
devmridul <devmridulsingh@gmail.com>
2022-06-18 17:55:44 +1000

    Fixed return type check error case in test file

--- src/channels.test.js
+++ src/channels.test.js
@@ -7,13 +7,13 @@ describe('Tests for channelsCreateV1', () => {
 		clearV1();
 		const authUserId = authRegisterV1('mal@gmail.com', '123456', 'first', 'last');
 		const authUserId2 = authRegisterV1('mal2@gmail.com', '123456', 'first', 'last');
-		expect(typeof authUserId).toBe('integer');
+		expect(typeof authUserId).toBe('object');
 		const channelId = channelsCreateV1(authUserId, 'name', 'true');
 		const channelId2 = channelsCreateV1(authUserId, 'name', 'true');
 		const channelId3 = channelsCreateV1(authUserId2, 'name', 'true');
-		expect(typeof channelId).toBe('integer');
-		expect(typeof channelId2).toBe('integer');
-		expect(typeof channelId3).toBe('integer');
+		expect(typeof channelId).toBe('object');
+		expect(typeof channelId2).toBe('object');
+		expect(typeof channelId3).toBe('object');
 	}); 
 
 	test('Testing error in creating channels by having no names or more than 20 characters', () => {
@@ -22,7 +22,7 @@ describe('Tests for channelsCreateV1', () => {
 		const channelId_a = channelsCreateV1(authUserId, '', true);
 		const channelId_b = channelsCreateV1(authUserId, '12345678912345678912345', true);
 		const channelId_c = channelsCreateV1('', 'name', 'true');
-		const channelId_d = channelsCreateV1(authUserId2, 'name', 'what');
+		const channelId_d = channelsCreateV1(authUserId, 'name', 'what');
 		expect(channelId_a).toStrictEqual({ error: 'error' });
 		expect(channelId_b).toStrictEqual({ error: 'error' });
 		expect(channelId_c).toStrictEqual({ error: 'error' });
