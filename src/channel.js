45007e43d744285b97028a67efac93e8884e927e
Phot <z5387411@ad.unsw.edu.au>
2022-06-22 03:57:04 +1000

    all fixed

--- src/channel.js
+++ src/channel.js
@@ -1,4 +1,4 @@
-import { getData } from './dataStore.js';
+import { getData, setData } from './dataStore.js';
 import { checkAuthUserId, checkChannelId, checkIfMember, authInChannel, getMessages } from './channelHelperFunctions.js';
 
 const ERROR = {
