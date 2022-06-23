62d1428f7be1657157a1c380aae3874db9c3efaf
Jacinta <z5308921@unsw.edu.au>
2022-06-24 09:09:59 +1000

    added function comment to other.js

--- src/other.js
+++ src/other.js
@@ -2,6 +2,16 @@ import {getData, setData} from './dataStore.js';
 
 function clearV1() {
 
+  	/*
+		Description:
+			clearV1 clears the dataStore before each jest test. 
+    
+		Arguments:
+			
+		Return Value:
+
+	*/
+
   let data = getData();
   
   data['users'] = [];
