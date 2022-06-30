4cd6decafce18abf48e75aa75c4514d29839af63
devmridul <devmridulsingh@gmail.com>
2022-06-30 21:33:56 +1000

    Changed other.js to Typescript

---
+++ src/other.ts
@@ -0,0 +1,21 @@
+import { getData, setData } from './dataStore';
+
+function clearV1() {
+  /*
+    Description:
+        clearV1 clears the dataStore
+
+    Arguments:
+
+    Return Value:
+
+  */
+  const data: any = getData();
+
+  data.users = [];
+  data.channels = [];
+
+  setData(data);
+}
+
+export { clearV1 };
